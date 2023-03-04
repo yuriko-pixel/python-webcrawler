3fb707c633c3fc16738d0095df4b3f20daa8a4ed
Yuriko Darragh <55519118+yuriko-pixel@users.noreply.github.com>
2023-03-04 19:06:31 +0900

    added url

--- pythonwebcrawler/spiders/crawling_spider.py
+++ pythonwebcrawler/spiders/crawling_spider.py
@@ -3,3 +3,9 @@ from scrapy.linkextractors import LinkExtractor
 
 class CrawlingSpider(CrawlSpider):
     name = "mycrawler"
+    allowed_domains = ["toscrape.com"]
+    start_url = ["http://books.toscrape.com/"]
+    
+    rules = (
+        Rule(LinkExtractor(allow="catalog/category")),
+    )
